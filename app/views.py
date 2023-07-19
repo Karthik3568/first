1a935c9204edc135780c1b87f0375176820609fb
Karthik3568 <karthikmanoj1231@gmail.com>
2023-07-19 15:30:31 +0200

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def Karthik(request):
+    return HttpResponse('<h1><marquee>Welcome to  my page</marquee></h1>')
+
+# Create your views here.
